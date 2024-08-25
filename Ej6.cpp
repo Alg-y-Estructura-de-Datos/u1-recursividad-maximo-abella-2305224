2642d7e99f3513f34312ba42066feb0c08d94b6b
Max20050 <maximoabella12@gmail.com>
2024-08-25 14:28:02 -0300

    recursividad 2

--- Ej6.cpp
+++ Ej6.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
+ 
+// EJERCICIO A TERMINAR
 
-bool contarpares(int numero,bool par) {
+
+bool contarpares(int numero,int pos) {
     if (numero == 0) {
         return;
     }
+    int x,x1,x2;
+    x = numero;
+    while (x >= 10){x = x/10;} //  x1 = 1,234
+    
+
+
+}
+
+void inttovect(int numero,int vector[]) {
+    int i = 0,x;
+    while (numero > 0)
+    {
+        x = 
+        vector[i] = x;
+    }
+    
 }
 
 
 
 int main() {
-    
     std::cout<<"Programacion 3 recursividad ejercicio 6: "<<std::endl;
     int numero;
     std::cout<<"Ingrese un numero"<<std::endl;
-    
+    std::cin>>numero;
+
 
 
     return 0;
